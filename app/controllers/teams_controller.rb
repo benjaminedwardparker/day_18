a5ddd86d58f904e51ab82f706545489386f5b397
Benjamin Parker <benjaminparker@gmail.com>
2014-10-16 04:07:58 -0500

    controller now has strong parameters

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,74 +1,55 @@
 class TeamsController < ApplicationController
-  before_action :set_team, only: [:show, :edit, :update, :destroy]
+#  before_action :set_team, only: [:show, :edit, :update, :destroy]
 
-  # GET /teams
-  # GET /teams.json
+
   def index
     @teams = Team.all
   end
 
-  # GET /teams/1
-  # GET /teams/1.json
+
   def show
+    @team = Team.find(params[:id])
   end
 
-  # GET /teams/new
+
   def new
-    @team = Team.new
+    @team = Team.new(coach: "Default")
   end
 
-  # GET /teams/1/edit
+
   def edit
+    @team = Team.find(params[:id])
   end
 
-  # POST /teams
-  # POST /teams.json
+
   def create
     @team = Team.new(team_params)
 
-    respond_to do |format|
-      if @team.save
-        format.html { redirect_to @team, notice: 'Team was successfully created.' }
-        format.json { render :show, status: :created, location: @team }
-      else
-        format.html { render :new }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
-      end
+    if @team.save
+      redirect_to teams_path, notice: "Team saved!"
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /teams/1
-  # PATCH/PUT /teams/1.json
+
   def update
-    respond_to do |format|
-      if @team.update(team_params)
-        format.html { redirect_to @team, notice: 'Team was successfully updated.' }
-        format.json { render :show, status: :ok, location: @team }
-      else
-        format.html { render :edit }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
-      end
+    @team = Team.find(params[:id])
+    if @team.update(team_params)
+      redirect_to @team, notice: "Team saved!"
+    else
+      render :edit
     end
   end
 
-  # DELETE /teams/1
-  # DELETE /teams/1.json
   def destroy
-    @team.destroy
-    respond_to do |format|
-      format.html { redirect_to teams_url, notice: 'Team was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @taco = Taco.find(params[:id])
+    @taco.destroy
+    redirect_to tacos_path, notice: "TEAM DESTROYED"
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_team
-      @team = Team.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def team_params
-      params.require(:team).permit(:name, :city, :bestplayer)
-    end
+  def team_params
+    params.require(:team).permit(:name, :city, :bestplayer)
+  end
 end
